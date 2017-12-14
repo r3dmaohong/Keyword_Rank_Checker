915ede81d28de474006da16d4ffbb9e351b1a159
r3dmaohong <r3dmaohong@gmail.com>
2017-12-14 15:46:47 +0800

    turn to docker

--- selenium4kwordrank.R
+++ selenium4kwordrank.R
@@ -1,169 +1,45 @@
 #'
 #' @title Checking keyword and ad rankings positions in yahoo and google
 #' @author r3dmaohong
-#' @keywords Rselenium
+#' @keywords Rselenium, docker
 
 #' @note It's just for 'ASSISTING' manual processing. 
 #'       Maybe it will be detected as bot.
 #'       Therefore, I add many inefficient code blocks for google checker...
 #'       Nevertheless, I still can't guarantee that google won't detected as BOT.
 
-# Version
-## Firefox: 47.0.2 (https://ftp.mozilla.org/pub/firefox/releases/47.0.2/win32/zh-TW/Firefox%20Setup%2047.0.2.exe)
-## Selenium sever: 2.53.1
-
-## Run selenium server
-# cmd
-# java -jar selenium-server-standalone-2.53.1.jar
-#selenium-server-standalone-3.4.0.jar
 library(RSelenium)
 library(rvest)
 library(httr)
-library(tcltk)
 library(gmailr)
+library(tcltk)
+library(clipr)
 
-setwd("git/keyword_rank_checker")
+setwd("workspace/datascience/r/crawler/keyword_rank_checker")
+
+#########################
+## machine name: default
+#########################
+#' docker-machine start default
+#' docker run -d -p 4445:4444 selenium/standalone-chrome
+#' docker ps
+#' docker-machine ip
+#' 
 
 #' Settings for browser
 ######################## 
-remDr <- remoteDriver(remoteServerAddr = "localhost"
-                      , port = 4444
-                      , browserName ="firefox"
-)
-#remDr$open() #open browser
-#remDr$getStatus()#check the status of browser
-
-#https://stackoverflow.com/questions/40317920/how-to-clear-history-in-rseleniums-internal-web-browser
-#https://stackoverflow.com/questions/33224070/how-to-open-incognito-private-window-with-selenium-wd-for-different-browser-type
-fprof <- makeFirefoxProfile(
-  list(
-    "browser.cache.disk.enable" = FALSE,
-    "browser.cache.memory.enable" = FALSE,
-    "browser.cache.offline.enable" = FALSE,
-    "network.http.use-cache" = FALSE,
-    "browser.privatebrowsing.autostart" = TRUE
-    
-  )
-)
-remDr <- remoteDriver(extraCapabilities = fprof)
-remDr$open()
+remDr <- remoteDriver(browserName = "chrome",
+                      remoteServerAddr = "192.168.99.100",
+                      port = 4445L
+                      )
+#remDr$open(silent=TRUE)
 ########################
 
 #' Import Keywords
-dat <- readClipboard()
-
+dat <- read_clip()
+dat
 #' Website's url
 grepltext = "hs.1111"
-
-#' YAHOO Checker
-###########
-yahoo_start = Sys.time()
-
-output_yahoo <- data.frame('query' = dat, 'ad' = "NA", 'rank' = "NA", stringsAsFactors = F)
-# Yahoo's ad got three places.
-ad_place = c("上", "右", "下")
-
-for(i in 1:length(dat)){
-  page  = 1
-  query = dat[i]
-  url   = paste0("https://tw.search.yahoo.com/search?fr=yfp-search-sb&p=", iconv(query, to="UTF-8"))
-  
-  remDr$navigate(url)
-  status = T
-  
-  #remDr$deleteAllCookies()
-  
-  while(status){
-    status = F
-    page_source <- remDr$getPageSource()
-    html_source <- read_html(page_source[[1]])
-    
-    # Yahoo is updating their website these days...
-    #links <- html_source %>% html_nodes(".wr-bw") %>%  html_text()
-    #links <- html_source %>% html_nodes(".fw-m") %>%  html_text()
-    #links <- html_source %>% html_nodes(".compTitle") %>%  html_text()
-    #links <- html_source %>% html_nodes("ol li .algo-sr span") %>%  html_text()
-    links <- html_source %>% html_nodes(".algo-sr span") %>%  html_text()
-    links <- links[links!="-"]
-    
-    sep_nodes <- html_source %>% html_nodes(".clrGrey") %>% html_text() %>% unique
-    sep_nodes <- sep_nodes[sep_nodes!="-"]
-    del_nodes <- c(which(links %in% sep_nodes), 
-                   which(links %in% sep_nodes) + 1,
-                   which(links %in% (html_source %>% html_nodes(".d-b") %>% html_text())),
-                   which(links %in% (html_source %>% html_nodes(".fz-s") %>% html_text())))
-    
-    if(toString(del_nodes)!="")
-      links <- links[-del_nodes]
-    links <- links[!grepl("^.[0-9].天前", links)]
-    #links <- links[grepl("[0-9a-zA-Z]+[.][0-9a-zA-Z]+", links)]
-    
-    if(length(links)!=10){
-      tkmessageBox(title = "Warning!",
-                   message = "Check the CSS, maybe it's updated!", 
-                   icon = "warning", 
-                   type = "ok") 
-    }
-    
-    top    <- html_source %>% html_nodes(".searchCenterTopAds .layoutMiddle") %>%  html_text() # .compText
-    right  <- html_source %>% html_nodes(".searchRightBottomAds .layoutMiddle") %>%  html_text() #.layoutCenter
-    bottom <- html_source %>% html_nodes(".searchCenterBottomAds .layoutMiddle") %>%  html_text()
-    
-    rank_links    <- which(grepl(grepltext, links, fixed = T))
-    rank_topad    <- which(grepl(grepltext, top, fixed = T)) %>% toString() %>% as.numeric()
-    rank_rightad  <- which(grepl(grepltext, right, fixed = T)) %>% toString() %>% as.numeric()
-    rank_bottomad <- which(grepl(grepltext, bottom, fixed = T)) %>% toString() %>% as.numeric()
-    
-    ad_pos <- c(rank_topad, rank_rightad, rank_bottomad)
-    
-    pos <- which(ad_pos == min(c(rank_topad, rank_rightad, rank_bottomad), na.rm = T))
-    output_yahoo$query[i] = query
-    
-    if(grepl("NA", toString(output_yahoo$ad[i])) | grepl("Inf", toString(output_yahoo$ad[i]))){
-      output_yahoo$ad[i] = paste0(page, "-", ad_place[pos][1], "-", ad_pos[pos[1]])
-    }
-    if(grepl("NA", toString(output_yahoo$rank[i]), fixed = T)){
-      output_yahoo$rank[i] = paste0(page, "-", rank_links[1])
-    }
-    Sys.sleep(runif(1,3,6))
-    
-    if(page < 3){#(toString(rank_links)=="" & page<=6){
-      status = T
-      webElem <- remDr$findElement("css selector", ".next")
-      remDr$mouseMoveToLocation(webElement = webElem) # move to the required element
-      remDr$click() # left mouse button click
-      page = page + 1 
-    }else{
-      status = F
-    }
-  }
-  print(i)
-  print(query)
-  print(output_yahoo$ad[i])
-  print(output_yahoo$rank[i])
-  Sys.sleep(runif(1,5,10))
-  
-  #remDr$quit()
-  #remDr$open()
-}
-output_yahoo$ad[grepl("NA", output_yahoo$ad, fixed = T)]      <- "x"
-output_yahoo$rank[grepl("NA", output_yahoo$rank, fixed = T)]  <- "x"
-output_yahoo$ad[grepl("Inf", output_yahoo$ad, fixed = T)]     <- "x"
-output_yahoo$rank[grepl("Inf", output_yahoo$rank, fixed = T)] <- "x"
-
-#' Making output as string when exporting to excel.
-output_yahoo$rank <- paste0("'", output_yahoo$rank)
-output_yahoo$ad   <- paste0("'", output_yahoo$ad)
-write.csv(output_yahoo, 
-          paste0("output/yahoo_", format(Sys.time(), "%Y_%m_%d_%H%M%S"), ".csv"), 
-          row.names = F)
-
-yahoo_end = Sys.time()
-
-yahoo_end - yahoo_start
-###########
-
-
 
 #' GOOGLE Checker
 ###########
@@ -280,6 +156,7 @@ output_google$rank[grepl("Inf", output_google$rank, fixed = T)] <- "x"
 output_google$rank <- paste0("'", output_google$rank)
 output_google$ad   <- paste0("'", output_google$ad)
 
+dir.create('output', showWarnings = FALSE)
 google_fn <- paste0("output/google_", format(Sys.time(), "%Y_%m_%d_%H%M%S"), ".csv")
 write.csv(output_google, 
           google_fn, 
@@ -289,12 +166,15 @@ google_end = Sys.time()
 google_end - google_start
 
 html_msg <- mime() %>%
-  to(c("@gmail.com", "@gmail.com")) %>%
+  to(c("@gmail.com")) %>%
   from("@gmail.com") %>%
   html_body("")
 file_attachment <- html_msg %>%
-  subject("Keyword Rank Check From BOT Mao") %>%
+  subject("Keyword Rank Check") %>%
   attach_file(google_fn)
 
 send_message(file_attachment)
 ###########
+
+#' docker-machine stop default
+#' docker-machine ls
