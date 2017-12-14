230bc0ab784d6c59e3f1d0ac52b4ff1c28291736
r3dmaohong <r3dmaohong@gmail.com>
2017-12-14 17:54:55 +0800

    turn to function

--- selenium4kwordrank.R
+++ selenium4kwordrank.R
@@ -32,149 +32,157 @@ remDr <- remoteDriver(browserName = "chrome",
                       remoteServerAddr = "192.168.99.100",
                       port = 4445L
                       )
-#remDr$open(silent=TRUE)
 ########################
 
 #' Import Keywords
-dat <- read_clip()
-dat
+( dat <- read_clip() )
+
 #' Website's url
 grepltext = "hs.1111"
 
 #' GOOGLE Checker
 ###########
-google_start = Sys.time()
-
-output_google <- data.frame('query' = dat, 'ad' = "NA", 'rank' = "NA", stringsAsFactors = F)
-
-#' Stop the program when been detected as bot by google.
-sorry_break <- FALSE
-
-#' Main program
-if(TRUE){
-  for(i in 1:length(dat)){
-    page  = 1
-    query = dat[i]
-    
-    remDr$open(silent = TRUE)
-    
-    if(TRUE){#i == 1){
-      remDr$navigate("https://www.google.com.tw/")
-    }
-    webElem_sch <- remDr$findElement(using = 'css',
-                                     value = '#lst-ib')
-    webElem_sch$clearElement()
-    Sys.sleep(runif(1, 2, 5))
-    webElem_sch$sendKeysToElement(list(query))
-    Sys.sleep(runif(1, 1, 3))
-    webElem_sch$sendKeysToElement(list(key = 'enter'))
-    
-    #url = paste0("https://www.google.com.tw/search?q=", iconv(query, to="UTF-8"))
-    #remDr$navigate(url)# website to crawl
-    status = T
-    remDr$deleteAllCookies()
-    
-    while(status){
-      #' Bot verification. Stop!
-      if(grepl("sorry", unlist(remDr$getCurrentUrl()))){
-        sorry_break = TRUE
+google_keyword_ad_checker <- function(remDr, dat, grepltext, mail_from, mail_to){
+  print('Start')
+  google_start = Sys.time()
+  
+  output_google <- data.frame('query' = dat, 'ad' = "NA", 'rank' = "NA", stringsAsFactors = F)
+  
+  #' Stop the program when been detected as bot by google.
+  sorry_break <- FALSE
+  
+  #' Main program
+  if(TRUE){
+    for(i in 1:length(dat)){
+      page  = 1
+      query = dat[i]
+      
+      remDr$open(silent = TRUE)
+      
+      if(TRUE){#i == 1){
+        remDr$navigate("https://www.google.com.tw/")
+      }
+      webElem_sch <- remDr$findElement(using = 'css',
+                                       value = '#lst-ib')
+      webElem_sch$clearElement()
+      Sys.sleep(runif(1, 2, 5))
+      webElem_sch$sendKeysToElement(list(query))
+      Sys.sleep(runif(1, 1, 3))
+      webElem_sch$sendKeysToElement(list(key = 'enter'))
+      
+      #url = paste0("https://www.google.com.tw/search?q=", iconv(query, to="UTF-8"))
+      #remDr$navigate(url)# website to crawl
+      status = T
+      remDr$deleteAllCookies()
+      
+      while(status){
+        #' Bot verification. Stop!
+        if(grepl("sorry", unlist(remDr$getCurrentUrl()))){
+          sorry_break = TRUE
+          break
+        }
+        
+        webElem <- remDr$findElement("css", "body")
+        
+        # I hate been detected as BOT...
+        #rnd_down_times <- floor(runif(1, 2, 4))
+        for(i_down_times in 1:3){#rnd_down_times){
+          Sys.sleep(runif(1, 0.5, 5))
+          webElem$sendKeysToElement(list(key = "page_down"))
+        }
+        
+        status = F
+        page_source <- remDr$getPageSource()
+        html_source <- read_html(page_source[[1]])
+        
+        links <- html_source %>% html_nodes("._Rm") %>%  html_text()# %>% html_nodes(".srg ._Rm") %>%  html_text()
+        ad    <- html_source %>% html_nodes(".ads-ad") %>%  html_text() # .compText
+        
+        rank_links <- which(grepl(grepltext, links, fixed = T))
+        rank_ad    <- which(grepl(grepltext, ad, fixed = T))
+        
+        output_google$query[i] = query
+        
+        if(grepl("NA", toString(output_google$ad[i])) | grepl("Inf", toString(output_google$ad[i]))){
+          output_google$ad[i] = paste0(page, "-", min(rank_ad, na.rm = T))
+        }
+        if(grepl("NA", toString(output_google$rank[i]), fixed = T)){
+          output_google$rank[i] = paste0(page, "-", rank_links[1])
+        }
+        Sys.sleep(runif(1, 5, 10))
+        
+        if(page < 3){#(toString(rank_links)=="" & page<=6){
+          status = T
+          webElem <- remDr$findElement("css selector", "#pnnext")
+          remDr$mouseMoveToLocation(webElement = webElem) # move to the required element
+          remDr$click() # left mouse button click
+          page = page + 1 
+        }else{
+          status = F
+          webElem$sendKeysToElement(list(key = "home"))
+        }
+      }
+      if(sorry_break==TRUE){
+        print("STOP! Been detected as BOT!")
         break
       }
+      print(i)
+      print(query)
+      print(output_google$ad[i])
+      print(output_google$rank[i])
+      Sys.sleep(runif(1, 10, 23))
       
-      webElem <- remDr$findElement("css", "body")
-      
-      # I hate been detected as BOT...
-      #rnd_down_times <- floor(runif(1, 2, 4))
-      for(i_down_times in 1:3){#rnd_down_times){
-        Sys.sleep(runif(1, 0.5, 5))
-        webElem$sendKeysToElement(list(key = "page_down"))
-      }
-      
-      status = F
-      page_source <- remDr$getPageSource()
-      html_source <- read_html(page_source[[1]])
-      
-      links <- html_source %>% html_nodes("._Rm") %>%  html_text()# %>% html_nodes(".srg ._Rm") %>%  html_text()
-      ad    <- html_source %>% html_nodes(".ads-ad") %>%  html_text() # .compText
-      
-      rank_links <- which(grepl(grepltext, links, fixed = T))
-      rank_ad    <- which(grepl(grepltext, ad, fixed = T))
-      
-      output_google$query[i] = query
-      
-      if(grepl("NA", toString(output_google$ad[i])) | grepl("Inf", toString(output_google$ad[i]))){
-        output_google$ad[i] = paste0(page, "-", min(rank_ad, na.rm = T))
-      }
-      if(grepl("NA", toString(output_google$rank[i]), fixed = T)){
-        output_google$rank[i] = paste0(page, "-", rank_links[1])
-      }
-      Sys.sleep(runif(1, 5, 10))
-      
-      if(page < 3){#(toString(rank_links)=="" & page<=6){
-        status = T
-        webElem <- remDr$findElement("css selector", "#pnnext")
-        remDr$mouseMoveToLocation(webElement = webElem) # move to the required element
-        remDr$click() # left mouse button click
-        page = page + 1 
-      }else{
-        status = F
-        webElem$sendKeysToElement(list(key = "home"))
-      }
+      tryCatch({
+        remDr$quit()
+      }, error = function(e) {
+        print("Browser closed.")
+      })
     }
-    if(sorry_break==TRUE){
-      print("STOP! Been detected as BOT!")
-      break
-    }
-    print(i)
-    print(query)
-    print(output_google$ad[i])
-    print(output_google$rank[i])
-    Sys.sleep(runif(1, 10, 23))
     
-    tryCatch({
-      remDr$quit()
-    }, error = function(e) {
-      print("Browser closed.")
-    })
+    
+    ## Alert
+    if(sorry_break==TRUE){
+      tkmessageBox(title = "STOP!",
+                   message = "Been detected as BOT!", 
+                   icon = "warning", 
+                   type = "ok")
+      print(sprintf("i = %s", i))
+    }
   }
   
+  output_google$ad[grepl("NA", output_google$ad, fixed = T)]      <- "x"
+  output_google$rank[grepl("NA", output_google$rank, fixed = T)]  <- "x"
+  output_google$ad[grepl("Inf", output_google$ad, fixed = T)]     <- "x"
+  output_google$rank[grepl("Inf", output_google$rank, fixed = T)] <- "x"
+  output_google$rank <- paste0("'", output_google$rank)
+  output_google$ad   <- paste0("'", output_google$ad)
   
-  ## Alert
-  if(sorry_break==TRUE){
-    tkmessageBox(title = "STOP!",
-                 message = "Been detected as BOT!", 
-                 icon = "warning", 
-                 type = "ok")
-    print(sprintf("i = %s", i))
-  }
+  dir.create('output', showWarnings = FALSE)
+  google_fn <- paste0("output/google_", format(Sys.time(), "%Y_%m_%d_%H%M%S"), ".csv")
+  write.csv(output_google, 
+            google_fn, 
+            row.names = F)
+  
+  google_end <- Sys.time()
+  print(google_end - google_start)
+  
+  html_msg <- mime() %>%
+    to(mail_to) %>%
+    from(mail_from) %>%
+    html_body("")
+  file_attachment <- html_msg %>%
+    subject("[noreply] Keyword Ranking Result in Google") %>%
+    attach_file(google_fn)
+  
+  send_message(file_attachment)
+  
+  return(output_google)
 }
-
-output_google$ad[grepl("NA", output_google$ad, fixed = T)]      <- "x"
-output_google$rank[grepl("NA", output_google$rank, fixed = T)]  <- "x"
-output_google$ad[grepl("Inf", output_google$ad, fixed = T)]     <- "x"
-output_google$rank[grepl("Inf", output_google$rank, fixed = T)] <- "x"
-output_google$rank <- paste0("'", output_google$rank)
-output_google$ad   <- paste0("'", output_google$ad)
-
-dir.create('output', showWarnings = FALSE)
-google_fn <- paste0("output/google_", format(Sys.time(), "%Y_%m_%d_%H%M%S"), ".csv")
-write.csv(output_google, 
-          google_fn, 
-          row.names = F)
-
-google_end = Sys.time()
-google_end - google_start
-
-html_msg <- mime() %>%
-  to(c("@gmail.com")) %>%
-  from("@gmail.com") %>%
-  html_body("")
-file_attachment <- html_msg %>%
-  subject("Keyword Rank Check") %>%
-  attach_file(google_fn)
-
-send_message(file_attachment)
 ###########
+
+result <- google_keyword_ad_checker(remDr, dat, grepltext, 
+                             mail_from = '@gmail.com', 
+                             mail_to = '@gmail.com')
 
 #' docker-machine stop default
 #' docker-machine ls
